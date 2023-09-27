c69535a9afef6c2f3709a42b57a905b68e0b6fab
IrynaGren <gren.ira10@gmail.com>
2023-09-28 01:01:59 +0300

    Modify project

---
+++ Event-Driven Programming Lesson/scripts.js
@@ -0,0 +1,7 @@
+// Function to display an alert
+function showAlert() {
+    alert("Button clicked! This is an alert message.");
+}
+
+// Adding an event listener to the button
+document.getElementById("myButton").addEventListener("click", showAlert);
---
+++ My Interests/scripts.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Event-Driven Programming Lesson</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <h1>Event-Driven Programming Lesson</h1>
+    <div id="div1">This is div 1</div>
+    <div id="div2">This is div 2</div>
+    
+    <input type="button" id="myButton" value="Click Me">
+    
+    <script src="script.js"></script>
+</body>
+</html>
