f459c90e8dd63cd43808162b705eae1df0c636e5
IrynaGren <gren.ira10@gmail.com>
2023-09-28 13:06:53 +0300

    Add changes

--- Upload and Display an Image/scripts.js
+++ Upload and Display an Image/scripts.js
@@ -1,31 +1,32 @@
 function upload() {
     // Get the file input element
-    const fileInput = document.getElementById('fileInput');
-
-    // Get the canvas element
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
+    const imageInput = document.getElementById("imageInput");
 
     // Check if a file was selected
-    if (fileInput.files.length > 0) {
-        // Create a SimpleImage from the chosen file
-        const file = fileInput.files[0];
-        const img = new SimpleImage(file);
+    if (imageInput.files.length === 0) {
+        alert("Please select an image to upload.");
+        return;
+    }
+
+    // Create a SimpleImage from the chosen file
+    const file = imageInput.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function (event) {
+        const image = new SimpleImage(event.target.result);
+        const canvas = document.getElementById("canvas");
+        const ctx = canvas.getContext("2d");
 
         // Clear the canvas
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        // Draw the image on the canvas while maintaining its aspect ratio
-        if (img.width > img.height) {
-            const aspectRatio = img.height / img.width;
-            const newWidth = canvas.width;
-            const newHeight = canvas.width * aspectRatio;
-            ctx.drawImage(img, 0, 0, newWidth, newHeight);
-        } else {
-            const aspectRatio = img.width / img.height;
-            const newWidth = canvas.height * aspectRatio;
-            const newHeight = canvas.height;
-            ctx.drawImage(img, 0, 0, newWidth, newHeight);
-        }
-    }
+        // Set the canvas dimensions to match the image
+        canvas.width = image.getWidth();
+        canvas.height = image.getHeight();
+
+        // Draw the image on the canvas
+        image.drawTo(canvas);
+    };
+
+    reader.readAsDataURL(file);
 }
